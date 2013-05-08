2d5f49f26752ed0f60b0851ed5dcc50ef8941759
OHTA Shogo <athos0220@gmail.com>
2013-05-08 23:18:01 +0900

    「安定な結婚の問題」を追加

---
+++ src/algorithm_exercise/stable_marriage.clj
@@ -0,0 +1,24 @@
+(ns algorithm-exercise.stable-marriage)
+
+(defn ^:private prefer? [prefs x y]
+  (let [m (into {} (map-indexed #(vector %2 %1) prefs))]
+    (< (m x) (m y))))
+
+(defn ^:private shift-prefs [prefs who]
+  (assoc prefs who (rest (prefs who))))
+
+(defn solve [men's-prefs women's-prefs]
+  (letfn
+    [(solve' [[man & more :as men] men's-prefs engaged]
+       (if (empty? men)
+         engaged
+         (let [woman (first (men's-prefs man))
+               woman-pref (women's-prefs woman)]
+           (if-let [suitor (engaged woman)]
+             (if (prefer? woman-pref man suitor)
+               (recur (cons suitor more)
+                      (shift-prefs men's-prefs suitor)
+                      (assoc engaged woman man))
+               (recur men (shift-prefs men's-prefs man) engaged))
+             (recur more men's-prefs (assoc engaged woman man))))))]
+    (solve' (keys men's-prefs) men's-prefs {})))
---
+++ test/algorithm_exercise/stable_marriage_test.clj
@@ -0,0 +1,12 @@
+(ns algorithm-exercise.stable-marriage-test
+  (:use algorithm-exercise.stable-marriage
+        [clojure.test :only (deftest testing is)]))
+
+(deftest test-solve
+  (testing "`solve' provides a solution for each stable marriage problem"
+    (is (= (solve {1 [1 3 2], 2 [2 3 1], 3 [3 1 2]}
+                  {1 [2 3 1], 2 [2 1 3], 3 [3 2 1]})
+           {1 1, 2 2, 3 3}))
+    (is (= (solve {1 [:a :b :c :d], 2 [:c :b :a :d], 3 [:a :b :d :c], 4 [:c :a :d :b]}
+                  {:a [1 2 3 4], :b [2 1 4 3], :c [2 3 1 4], :d [1 4 3 2]})
+           {:a 1, :b 3, :c 2, :d 4}))))
